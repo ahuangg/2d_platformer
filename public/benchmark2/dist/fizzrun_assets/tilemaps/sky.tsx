c724b1562bf2980ec7649b7db0da1007795d7004
Alan Huang <98438095+ahuangg@users.noreply.github.com>
2024-06-03 20:58:15 -0700

    Delete public/benchmark2 directory

--- public/benchmark2/dist/fizzrun_assets/tilemaps/sky.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="sky" tilewidth="32" tileheight="32" tilecount="80" columns="10">
- <transformations hflip="0" vflip="1" rotate="0" preferuntransformed="0"/>
- <image source="sky.png" width="320" height="256"/>
-</tileset>
